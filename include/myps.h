7eec6cb6ab819813bb649d07f4df5cdfdf464213
vraus <hadrien.belleville22@gmail.com>
2023-12-07 15:58:09 +0100

    :memo: added comments in myps

--- include/myps.h
+++ include/myps.h
@@ -21,8 +21,26 @@
         exit(status);             \
     } while (0)
 
+/**
+ * @brief Prints information about a process based on its process ID (PID).
+ *
+ * This function reads information from the /proc/<pid>/status and /proc/<pid>/cmdline files
+ * to retrieve details such as UID, PID, PPID, STAT, NAME, and COMMAND.
+ *
+ * @param pid `int` The process ID for which information is to be printed.
+ */
 void print_process_info(int pid);
 
+/**
+ * @brief Entry point for the myps program. Lists information about all processes on the system.
+ *
+ * This function opens the /proc directory, iterates through its entries, and prints information
+ * about each process using the print_process_info function.
+ *
+ * @param argc `int` Number of command-line arguments.
+ * @param argv `char *[]` Array of command-line argument strings.
+ * @return `int` Exit status of the program.
+ */
 int main(int argc, char *argv[]);
 
 #endif
