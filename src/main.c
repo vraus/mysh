3d1d1d8d61fd26010fd05028b3c9a4f03ccce61e
vraus <hadrien.belleville22@gmail.com>
2023-12-09 16:36:15 +0100

    :memo: Update comments in main.c

--- src/main.c
+++ src/main.c
@@ -72,6 +72,12 @@ void unset_local_variables(struct Variable *variables, char *_name)
     handle_error_noexit("Error: Variable not found.\n");
 }
 
+/**
+ * @brief Method to extract the name in a reference ($var)
+ *
+ * @param _name The variable reference (with '$')
+ * @return The var name without the '$' reference.
+ */
 char *extract_var_name(char *_name)
 {
     char *var_name = (char *)calloc(strlen(_name), sizeof(char));
@@ -126,9 +132,10 @@ void set_local_variable(struct Variable *variables, char *_name, char *_value)
 }
 
 /**
- * @brief Prints the local variables stored in the array
+ * @brief Prints the local variable stored in the array
  *
  * @param variables Pointer to the array of local variables
+ * @param _name Name of the variable you want to print its value
  */
 void print_variable(struct Variable *variables, char *_name)
 {
